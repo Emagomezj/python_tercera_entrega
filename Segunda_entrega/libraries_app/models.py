b5be2da2580ce36c424972a9a14b3d0db3cec1ae
Emagomezj <emagomezj@gmail.com>
2024-08-06 22:45:36 -0300

    agrego método str a los modelos

--- Segunda_entrega/libraries_app/models.py
+++ Segunda_entrega/libraries_app/models.py
@@ -6,6 +6,8 @@ class Users(models.Model):
     name = models.CharField(max_length=20)
     surname = models.CharField(max_length=20)
     email = models.EmailField(max_length=30)
+    def __str__(self):
+        return f'{self.name} {self.surname}'
     
 class Books(models.Model):
     name = models.CharField(max_length=40)
@@ -14,7 +16,11 @@ class Books(models.Model):
     editorial = models.CharField(max_length=40)
     edition = models.CharField(max_length=40)
     available = models.BooleanField()
+    def __str__(self):
+        return f'{self.name} by {self.author}'
 class Users_libraries(models.Model):
     user = models.ForeignKey(Users, on_delete=models.CASCADE)
     books = models.ManyToManyField(Books)
     available = models.BooleanField()
+    def __str__(self):
+        return f"{self.user}'s library"
